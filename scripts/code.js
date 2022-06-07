e3d78a3fedc5008cae3c7c65dd0a24dd59d95a03
MuddathirDawood <dmuddathir@gmail.com>
2022-06-07 16:05:33 +0200

    keyboard

---
+++ scripts/code.js
@@ -0,0 +1,89 @@
+
+function addChara(){
+    document.getElementById('output').innerHTML += 'a';
+  }
+  function addCharb(){
+    document.getElementById('output').innerHTML += 'b';
+  }
+  function addCharc(){
+    document.getElementById('output').innerHTML += 'c';
+  }
+  function addChard(){
+    document.getElementById('output').innerHTML += 'd';
+  }
+  function addChare(){
+    document.getElementById('output').innerHTML += 'e';
+  }
+  function addCharf(){
+    document.getElementById('output').innerHTML += 'f';
+  }
+  function addCharg(){
+    document.getElementById('output').innerHTML += 'g';
+  }
+  function addCharh(){
+    document.getElementById('output').innerHTML += 'h';
+  }
+  function addChari(){
+    document.getElementById('output').innerHTML += 'i';
+  }
+  function addCharj(){
+    document.getElementById('output').innerHTML += 'j';
+  }
+  function addChark(){
+    document.getElementById('output').innerHTML += 'k';
+  }
+  function addCharl(){
+    document.getElementById('output').innerHTML += 'l';
+  }
+  function addCharm(){
+    document.getElementById('output').innerHTML += 'm';
+  }
+  function addCharn(){
+    document.getElementById('output').innerHTML += 'n';
+  }
+  function addCharo(){
+    document.getElementById('output').innerHTML += 'o';
+  }
+  function addCharp(){
+    document.getElementById('output').innerHTML += 'p';
+  }
+  function addCharq(){
+    document.getElementById('output').innerHTML += 'q';
+  }
+  function addCharr(){
+    document.getElementById('output').innerHTML += 'r';
+  }
+  function addChars(){
+    document.getElementById('output').innerHTML += 's';
+  }
+  function addChart(){
+    document.getElementById('output').innerHTML += 't';
+  }
+  function addCharu(){
+    document.getElementById('output').innerHTML += 'u';
+  }
+  function addCharv(){
+    document.getElementById('output').innerHTML += 'v';
+  }
+  function addCharw(){
+    document.getElementById('output').innerHTML += 'w';
+  }
+  function addCharx(){
+    document.getElementById('output').innerHTML += 'x';
+  }
+  function addChary(){
+    document.getElementById('output').innerHTML += 'y';
+  }
+  function addCharz(){
+    document.getElementById('output').innerHTML += 'z';
+  }
+  function addSpace(){
+    document.getElementById('output').innerHTML += ' ';
+  }
+  function addEnter(){
+    document.getElementById('output').innerHTML += '<br>';
+  }
+
+  function delChar(){
+    document.getElementById('output').innerHTML = document.getElementById('output').innerHTML.slice(0, -1);
+  }
